96998d9c13dbc8859c5b78924e4f6dad764109e1
Wictor <wictorlopes258@gmail.com>
2023-01-06 17:47:46 -0300

    adição da instrução de login no README

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
-import { CreditDebitFilterPipe } from './creditDebitFilter'
 import { HttpClient } from '@angular/common/http'
 import { AuthService } from './logout'
 @Component({
